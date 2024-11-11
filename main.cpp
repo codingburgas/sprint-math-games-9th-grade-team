82ae1f99498d08b1d7a22b5749b564c8d04f4dca
Denis Zavalishin <DDZavalishin23@codingburgas.bg>
2024-11-11 11:14:18 +0200

    Add main.cpp file

---
+++ main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+ 
+int main(){
+    cout << "Main menu" << endl;
+    cout << "1. play" << endl;
+    cout << "2. About us"<< endl;
+    cout << "3. Leaderboard" << endl;
+    int mainchoose;
+    cin >> mainchoose;
+        switch(mainchoose){  // choosing what you want
+        case(1):{ 
+            //fucntion play
+            cout << choose;
+            break;
+        }
+        case(2):{
+            //fucntion about us
+            cout << choose;
+            break;
+        }  
+        case(3):{
+            //fucntion etc
+            cout << choose;
+            break;
+        }
+ 
+    }
+}
