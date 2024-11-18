1cecb69f5381c3c25b4ff0ff1b57f2a13fae97a5
ddzavalishin23 <ddzavalishin23@codingburgas.bg>
2024-11-18 21:05:42 +0300

    Add main game, fix bugs

--- main.cpp
+++ main.cpp
@@ -1,6 +1,58 @@
 #include <iostream>
 #include <string>
 using namespace std;
+void game(double levelmultiplier) {
+    int wavecount = 10;
+    for (int wavecounter = 0; wavecounter < wavecount;) {
+        double enemyHP = 100 * levelmultiplier, enemyDMG = 99 * levelmultiplier, playerHP = 100, playerDMG = 100;
+        while (enemyHP > 0) {
+            int timertime = 10;
+            int counter =    0, answer, rightanswer = 10;
+            char exitchar;
+
+            cout << "You have 3 try's" << endl;
+            cout << "Example!" << endl;
+            cout << "Type here your answer: ";
+            do {
+                cin >> answer;
+                counter++;
+                if (answer != rightanswer) {
+                    cout << "Wrong answer!" << endl;
+                }
+            }
+            while (answer != rightanswer && counter < 3);
+
+            if (answer == rightanswer) { // 
+                cout << "Good hit!" << endl;
+                enemyHP = enemyHP - playerDMG;
+                cout << "Enemy HP  == " << enemyHP << endl;
+            }
+            else {
+                cout << "You missed an attack!" << endl << "-"; //enemy damage
+                playerHP = playerHP - enemyDMG;
+                cout << "Your Hp == " << playerHP << endl;
+
+            }
+                if (enemyHP <= 0) { //if you kill the enemy next wave starts
+                    system("CLS");
+                    cout << "Wave cleared!"; 
+                    wavecounter++;
+                    break;
+
+                }
+                if (playerHP <= 0) {
+                    cout << "You Lose!, type anything to return to main menu!" << endl;
+                    cin >> exitchar;
+                    if (exitchar != '\0') { // exit to main menu
+                        wavecounter = 10;
+                        break;
+                    }
+                }
+            
+        }
+    }
+    
+}
 
 int main() {
 mainmenu: {
@@ -60,9 +112,18 @@ mainmenu: {
         } while (gamechoose < 1 || gamechoose > 5);
 
         switch (gamechoose) {
-        case 1: cout << "\n*** Easy Level Selected ***\n"; break;
-        case 2: cout << "\n*** Medium Level Selected ***\n"; break;
-        case 3: cout << "\n*** Hard Level Selected ***\n"; break;
+        case 1: {
+            game(1);
+            goto mainmenu;
+        }
+        case 2: {
+            game(1.5);
+            goto mainmenu;
+        }
+        case 3: {
+            game(2);
+            goto mainmenu;
+        }
         case 4: cout << "\n*** How to Play Instructions ***\n"; break;
         case 5: system("CLS"); goto mainmenu;
         }
